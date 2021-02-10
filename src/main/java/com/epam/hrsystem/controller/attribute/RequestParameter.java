77ebddc564be3c23d236501c604e0fbf63d3132e
Peachex <klevolex@gmail.com>
2021-02-10 23:05:01 +0300

    move to attribute

--- src/main/java/com/epam/hrsystem/controller/attribute/RequestParameter.java
+++ src/main/java/com/epam/hrsystem/controller/attribute/RequestParameter.java
@@ -1,15 +1,15 @@
-package com.epam.hrsystem.controller;
+package com.epam.hrsystem.controller.attribute;
 
 public class RequestParameter {
 
     // User
     public static final String FIRST_NAME = "firstName";
     public static final String LAST_NAME = "lastName";
-    public static final String DATE_OF_BIRTH = "dayOfBirth";
+    public static final String DATE_OF_BIRTH = "dateOfBirth";
     public static final String PHONE_NUMBER = "phoneNumber";
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
-    public static final String PASSWORD_REPEAT = "passwordRepeat";
+    public static final String REPEATED_PASSWORD = "repeatedPassword";
     public static final String PHOTO_NAME = "photoName";
 
     //Vacancy
--- src/main/java/com/epam/hrsystem/controller/attribute/SessionAttribute.java
+++ src/main/java/com/epam/hrsystem/controller/attribute/SessionAttribute.java
@@ -1,4 +1,4 @@
-package com.epam.hrsystem.controller;
+package com.epam.hrsystem.controller.attribute;
 
 public class SessionAttribute {
     public static final String USER_ID = "userId";
