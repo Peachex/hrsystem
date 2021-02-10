3929250b89f837d0a7816d98f9d28053a0545fd2
Peachex <klevolex@gmail.com>
2021-02-10 23:07:28 +0300

    create url pattern

---
+++ src/main/java/com/epam/hrsystem/controller/UrlPattern.java
@@ -0,0 +1,13 @@
+package com.epam.hrsystem.controller;
+
+public class UrlPattern {
+    public static final String CONTROLLER = "*.do";
+    public static final String UPLOAD_SERVLET = "*.upload";
+    public static final String HOME = "/home";
+    public static final String LOGIN = "/login";
+    public static final String VACANCY = "/vacancy";
+    public static final String REGISTER = "/register";
+
+    private UrlPattern() {
+    }
+}
