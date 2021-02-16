caa5990cebff8dcf284234fae711519ceb8df727
Peachex <klevolex@gmail.com>
2021-02-16 13:14:17 +0300

    fix bugs

--- src/main/java/com/epam/hrsystem/validator/BaseValidator.java
+++ src/main/java/com/epam/hrsystem/validator/BaseValidator.java
@@ -4,12 +4,13 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BaseValidator {
     private static final Logger logger = LogManager.getLogger();
-    private static final Pattern DATE_FORMAT_PATTERN = Pattern.compile("([1-9][0-9]{3})-([0][1-9]|[1][0-2])-([0][1-9]|[12][0-9]|[3][01])");
+    private static final Pattern DATE_FORMAT_PATTERN = Pattern.compile("([1-2][0-9]{3})-([0][1-9]|[1][0-2])-([0][1-9]|[12][0-9]|[3][01])");
 
     private BaseValidator() {
     }
@@ -19,7 +20,7 @@ public class BaseValidator {
             return false;
         }
         Matcher matcher = DATE_FORMAT_PATTERN.matcher(date);
-        boolean result = matcher.matches();
+        boolean result = matcher.matches() && LocalDate.parse(date).isBefore(LocalDate.now());
         if (!result) {
             logger.log(Level.DEBUG, "Date isn't valid: " + date);
         }
