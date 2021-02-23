da523dced5f626de08e165277a8fd0c234510fc6
Peachex <klevolex@gmail.com>
2021-02-23 12:14:37 +0300

    fix bugs

--- src/main/java/com/epam/hrsystem/controller/command/impl/EditVacancyInfoCommand.java
+++ src/main/java/com/epam/hrsystem/controller/command/impl/EditVacancyInfoCommand.java
@@ -8,6 +8,7 @@ import com.epam.hrsystem.controller.command.ActionCommand;
 import com.epam.hrsystem.controller.command.CommandResult;
 import com.epam.hrsystem.exception.CommandException;
 import com.epam.hrsystem.exception.ServiceException;
+import com.epam.hrsystem.model.entity.Vacancy;
 import com.epam.hrsystem.model.service.VacancyService;
 import com.epam.hrsystem.model.service.impl.VacancyServiceImpl;
 import org.apache.logging.log4j.Level;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class EditVacancyInfoCommand implements ActionCommand {
     private static final Logger logger = LogManager.getLogger();
@@ -48,7 +50,12 @@ public class EditVacancyInfoCommand implements ActionCommand {
                 request.setAttribute(RequestParameter.COUNTRY, fields.get(RequestParameter.COUNTRY));
                 request.setAttribute(RequestParameter.CITY, fields.get(RequestParameter.CITY));
                 request.setAttribute(Constant.ERROR_VACANCY_UPDATING_ATTRIBUTE, Constant.ERROR_VACANCY_UPDATING_MESSAGE);
-                result = new CommandResult(CommandName.TO_EMPLOYEE_VACANCY_INFO + vacancyIdStr, CommandResult.Type.FORWARD);
+                Optional<Vacancy> vacancyOptional = service.findVacancyById(vacancyId);
+                if (vacancyOptional.isPresent() && vacancyOptional.get().getEmployee().getId() == employeeId) {
+                    result = new CommandResult(CommandName.TO_EMPLOYEE_VACANCY_INFO + vacancyIdStr, CommandResult.Type.FORWARD);
+                } else {
+                    result = new CommandResult(CommandName.TO_EMPLOYEE_VACANCIES + employeeId, CommandResult.Type.REDIRECT);
+                }
             }
         } catch (ServiceException | NumberFormatException e) {
             logger.log(Level.ERROR, "Couldn't create vacancy");
