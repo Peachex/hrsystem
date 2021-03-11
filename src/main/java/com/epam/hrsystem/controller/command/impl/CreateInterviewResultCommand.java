217d0db506b7c01f4918ce4c2cfc62ff269440c4
Peachex <klevolex@gmail.com>
2021-03-11 21:11:01 +0300

    add interview result creation

--- src/main/java/com/epam/hrsystem/controller/command/impl/CreateInterviewResultCommand.java
+++ src/main/java/com/epam/hrsystem/controller/command/impl/CreateInterviewResultCommand.java
@@ -3,10 +3,12 @@ package com.epam.hrsystem.controller.command.impl;
 import com.epam.hrsystem.controller.attribute.CommandName;
 import com.epam.hrsystem.controller.attribute.JspAttribute;;
 import com.epam.hrsystem.controller.attribute.RequestParameter;
+import com.epam.hrsystem.controller.attribute.SessionAttribute;
 import com.epam.hrsystem.controller.command.ActionCommand;
 import com.epam.hrsystem.controller.command.CommandResult;
 import com.epam.hrsystem.exception.CommandException;
 import com.epam.hrsystem.exception.ServiceException;
+import com.epam.hrsystem.model.entity.ApplicantRequest;
 import com.epam.hrsystem.model.service.ApplicantRequestService;
 import com.epam.hrsystem.model.service.impl.ServiceHolder;
 import com.epam.hrsystem.validator.InterviewResultValidator;
@@ -16,8 +18,10 @@ import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class CreateInterviewResultCommand implements ActionCommand {
     private static final Logger logger = LogManager.getLogger();
@@ -39,15 +43,24 @@ public class CreateInterviewResultCommand implements ActionCommand {
 
         ApplicantRequestService applicantRequestService = ServiceHolder.HOLDER.getApplicantRequestService();
         CommandResult result;
-        long vacancyId = Long.parseLong(vacancyIdStr);
-        long applicantId = Long.parseLong(applicantIdStr);
         try {
+            long vacancyId = Long.parseLong(vacancyIdStr);
+            long applicantId = Long.parseLong(applicantIdStr);
             if (applicantRequestService.createInterviewResult(fields, vacancyId, applicantId)) {
                 result = new CommandResult(CommandName.TO_EMPLOYEE_APPLICANT_REQUEST + vacancyIdStr + APPLICANT_ID_COMMAND_PARAMETER +
                         applicantIdStr, CommandResult.Type.REDIRECT);
                 //todo add message sending
             } else {
-                result = new CommandResult(CommandResult.Type.RETURN_WITH_FORWARD);
+                HttpSession session = request.getSession();
+                long employeeId = (long) session.getAttribute(SessionAttribute.USER_ID);
+                Optional<ApplicantRequest> applicantRequest = applicantRequestService.findApplicantRequestByVacancyIdAndApplicantId(vacancyId, applicantId);
+                if (applicantRequest.isPresent() && applicantRequest.get().getVacancy().getEmployee().getId() == employeeId) {
+                    result = new CommandResult(CommandName.TO_EMPLOYEE_APPLICANT_REQUEST + vacancyIdStr + APPLICANT_ID_COMMAND_PARAMETER +
+                            applicantIdStr, CommandResult.Type.FORWARD);
+                } else {
+                    request.setAttribute(JspAttribute.NO_APPLICANT_REQUEST_ATTRIBUTE, JspAttribute.NO_APPLICANT_REQUEST_MESSAGE);
+                    result = new CommandResult(CommandName.TO_EMPLOYEE_VACANCIES + employeeId, CommandResult.Type.FORWARD);
+                }
                 if (!InterviewResultValidator.isInterviewResultFormValid(fields)) {
                     request.setAttribute(RequestParameter.INTERVIEW_RESULT_RATING, fields.get(RequestParameter.INTERVIEW_RESULT_RATING));
                     request.setAttribute(RequestParameter.INTERVIEW_RESULT_COMMENT, fields.get(RequestParameter.INTERVIEW_RESULT_COMMENT));
@@ -57,7 +70,7 @@ public class CreateInterviewResultCommand implements ActionCommand {
                             JspAttribute.ERROR_INTERVIEW_RESULT_DUPLICATE_CREATION_MESSAGE);
                 }
             }
-        } catch (ServiceException e) {
+        } catch (ServiceException | NumberFormatException e) {
             logger.log(Level.ERROR, "Couldn't create interview result: " + e);
             throw new CommandException(e);
         }
