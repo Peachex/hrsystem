4db8dde5892e904971507cffa3c911cd932c2d6c
Peachex <klevolex@gmail.com>
2021-03-23 20:31:05 +0300

    add new variable

--- src/main/java/com/epam/hrsystem/model/entity/UserReport.java
+++ src/main/java/com/epam/hrsystem/model/entity/UserReport.java
@@ -4,20 +4,23 @@ import java.time.LocalDate;
 
 public class UserReport {
     private long id;
+    private boolean isAvailable;
     private String subject;
     private String comment;
     private final LocalDate creationDate;
     private User user;
 
-    public UserReport(long id, String subject, String comment, LocalDate creationDate, User user) {
+    public UserReport(long id, boolean isAvailable, String subject, String comment, LocalDate creationDate, User user) {
         this.id = id;
+        this.isAvailable = isAvailable;
         this.subject = subject;
         this.comment = comment;
         this.creationDate = creationDate;
         this.user = user;
     }
 
-    public UserReport(String subject, String comment, LocalDate creationDate, User user) {
+    public UserReport(boolean isAvailable, String subject, String comment, LocalDate creationDate, User user) {
+        this.isAvailable = isAvailable;
         this.subject = subject;
         this.comment = comment;
         this.creationDate = creationDate;
@@ -26,6 +29,14 @@ public class UserReport {
 
     public long getId() {
         return id;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean available) {
+        isAvailable = available;
     }
 
     public String getSubject() {
@@ -65,7 +76,8 @@ public class UserReport {
             return false;
         }
         UserReport other = (UserReport) obj;
-        return (this.id == other.id) && (this.user != null ? this.user.equals(other.user) : other.user == null) &&
+        return (this.id == other.id && this.isAvailable == other.isAvailable) &&
+                (this.user != null ? this.user.equals(other.user) : other.user == null) &&
                 (this.subject != null ? this.subject.equals(other.subject) : other.subject == null) &&
                 (this.comment != null ? this.comment.equals(other.comment) : other.comment == null) &&
                 (this.creationDate != null ? this.creationDate.compareTo(other.creationDate) == 0 : other.creationDate == null);
@@ -74,6 +86,7 @@ public class UserReport {
     @Override
     public int hashCode() {
         int result = Long.hashCode(id);
+        result = 31 * result + Boolean.hashCode(isAvailable);
         result = 31 * result + (subject != null ? subject.hashCode() : 0);
         result = 31 * result + (comment != null ? comment.hashCode() : 0);
         result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
@@ -85,6 +98,7 @@ public class UserReport {
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("id = ").append(id).append("\n");
+        sb.append("is available = ").append(isAvailable).append("\n");
         sb.append("subject = ").append(subject).append("\n");
         sb.append("comment = ").append(comment).append("\n");
         sb.append("creation date = ").append(creationDate).append("\n");
