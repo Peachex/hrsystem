cb62b2a4ff5a274f12b043dd9407dce46cac8b07
Peachex <klevolex@gmail.com>
2021-04-05 20:15:12 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/model/entity/UserReport.java
+++ src/main/java/com/epam/hrsystem/model/entity/UserReport.java
@@ -2,6 +2,9 @@ package com.epam.hrsystem.model.entity;
 
 import java.time.LocalDate;
 
+/**
+ * Entity class represents a user report.
+ */
 public class UserReport {
     private long id;
     private boolean isAvailable;
@@ -11,9 +14,22 @@ public class UserReport {
     private String response;
     private User user;
 
+    /**
+     * Constructs a UserReport object.
+     */
     public UserReport() {
     }
 
+    /**
+     * Constructs a UserReport object with given id, status, subject, comment, creationDate, user.
+     *
+     * @param id           long value of user report's id.
+     * @param isAvailable  boolean value of user report's status.
+     * @param subject      String object of user report's subject.
+     * @param comment      String object of user report's comment.
+     * @param creationDate LocalDate object of user report's creation date.
+     * @param user         User object of user report's user.
+     */
     public UserReport(long id, boolean isAvailable, String subject, String comment, LocalDate creationDate, User user) {
         this.id = id;
         this.isAvailable = isAvailable;
@@ -23,6 +39,14 @@ public class UserReport {
         this.user = user;
     }
 
+    /**
+     * Constructs a UserReport object with given status, subject, comment, creationDate.
+     *
+     * @param isAvailable  boolean value of user report's status.
+     * @param subject      String object of user report's subject.
+     * @param comment      String object of user report's comment.
+     * @param creationDate LocalDate object of user report's creation date.
+     */
     public UserReport(boolean isAvailable, String subject, String comment, LocalDate creationDate) {
         this.isAvailable = isAvailable;
         this.subject = subject;
@@ -30,50 +54,110 @@ public class UserReport {
         this.creationDate = creationDate;
     }
 
+    /**
+     * Getter method of user report's id.
+     *
+     * @return long value of user report's id.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Getter method of user report's status.
+     *
+     * @return boolean value of user report's status.
+     */
     public boolean getIsAvailable() {
         return isAvailable;
     }
 
+    /**
+     * Setter method of user report's status.
+     *
+     * @param available boolean value of user report's status.
+     */
     public void setIsAvailable(boolean available) {
         isAvailable = available;
     }
 
+    /**
+     * Getter method of user report's subject.
+     *
+     * @return String object of user report's subject.
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * Setter method of user report's subject.
+     *
+     * @param subject String object of user report's subject.
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
+    /**
+     * Getter method of user report's comment.
+     *
+     * @return String object of user report's comment.
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * Setter method of user report's comment.
+     *
+     * @param comment String object of user report's comment.
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * Getter method of user report's creation date.
+     *
+     * @return LocalDate object of user report's creation date.
+     */
     public LocalDate getCreationDate() {
         return creationDate;
     }
 
+    /**
+     * Getter method of user report's response.
+     *
+     * @return String object of user report's response.
+     */
     public String getResponse() {
         return response;
     }
 
+    /**
+     * Setter method of user report's response.
+     *
+     * @param response String object of user report's response.
+     */
     public void setResponse(String response) {
         this.response = response;
     }
 
+    /**
+     * Getter method of user report's user.
+     *
+     * @return User object of user report's user.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter method of user report's user.
+     *
+     * @param user User object of user report's user.
+     */
     public void setUser(User user) {
         this.user = user;
     }
