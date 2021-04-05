0c0daa4115bf800366437304f2468cca0af96bc5
Peachex <klevolex@gmail.com>
2021-04-05 20:22:15 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/model/entity/ApplicantState.java
+++ src/main/java/com/epam/hrsystem/model/entity/ApplicantState.java
@@ -1,5 +1,23 @@
 package com.epam.hrsystem.model.entity;
 
+/**
+ * Enumeration of applicants' state.
+ */
 public enum ApplicantState {
-    LEFT_REQUEST, READY_FOR_TECHNICAL_INTERVIEW, PASSED, FAILED
+    /**
+     * Represents the state when applicant left a request.
+     */
+    LEFT_REQUEST,
+    /**
+     * Represents the state when applicant is ready for a technical interview.
+     */
+    READY_FOR_TECHNICAL_INTERVIEW,
+    /**
+     * Represents the state when applicant has passed all interviews.
+     */
+    PASSED,
+    /**
+     * Represents the state when applicant has failed an interview.
+     */
+    FAILED
 }
