9f5f79375e978e713fae3d948e9471a3d4628707
Aleksey Klevitov <klevolex@gmail.com>
2021-04-06 13:41:49 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/model/dao/InterviewResultDao.java
+++ src/main/java/com/epam/hrsystem/model/dao/InterviewResultDao.java
@@ -5,10 +5,36 @@ import com.epam.hrsystem.model.entity.InterviewResult;
 
 import java.util.Optional;
 
+/**
+ * Interface used for interactions with interview results table.
+ *
+ * @author Aleksey Klevitov
+ */
 public interface InterviewResultDao {
+    /**
+     * Adds interview result to the table.
+     *
+     * @param interviewResult InterviewResult object.
+     * @return boolean value. True if the interview result has been added, false otherwise.
+     * @throws DaoException if the database throws SQLException.
+     */
     boolean add(InterviewResult interviewResult) throws DaoException;
 
+    /**
+     * Finds interview result id by interview result.
+     *
+     * @param interviewResult InterviewResult object.
+     * @return Optional object of long value if exists, Optional.empty() otherwise.
+     * @throws DaoException if the database throws SQLException.
+     */
     Optional<Long> findInterviewResultId(InterviewResult interviewResult) throws DaoException;
 
+    /**
+     * Finds interview result by id.
+     *
+     * @param interviewResultId InterviewResult object.
+     * @return Optional object of interview result if exists, Optional.empty() otherwise.
+     * @throws DaoException if the database throws SQLException.
+     */
     Optional<InterviewResult> findInterviewResultById(long interviewResultId) throws DaoException;
 }
