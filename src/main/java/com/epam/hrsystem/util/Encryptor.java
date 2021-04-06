1d2f50a7054c1e47453e08ae1dbe308483b07cc6
Aleksey Klevitov <klevolex@gmail.com>
2021-04-06 15:04:01 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/util/Encryptor.java
+++ src/main/java/com/epam/hrsystem/util/Encryptor.java
@@ -2,16 +2,34 @@ package com.epam.hrsystem.util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * Class used to encrypt and check already encrypted passwords.
+ *
+ * @author Aleksey Klevitov
+ */
 public class Encryptor {
     private Encryptor() {
     }
 
+    /**
+     * Encrypts a given password.
+     *
+     * @param password String object of the password to be encrypted.
+     * @return String object of encrypted password.
+     */
     public static String encrypt(String password) {
         String salt = BCrypt.gensalt();
         String hashedValue = BCrypt.hashpw(password, salt);
         return hashedValue;
     }
 
+    /**
+     * Checks if an unencrypted password equals to an encrypted one.
+     *
+     * @param password    String object of unencrypted password.
+     * @param hashedValue String object of encrypted password.
+     * @return boolean value. True if the unencrypted password equals to the encrypted one, false otherwise.
+     */
     public static boolean check(String password, String hashedValue) {
         boolean result = BCrypt.checkpw(password, hashedValue);
         return result;
