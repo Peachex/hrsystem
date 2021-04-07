6573695ced503294ee377ac5f6324f852c86fe43
Aleksey Klevitov <klevolex@gmail.com>
2021-04-07 17:50:12 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/controller/attribute/SessionAttribute.java
+++ src/main/java/com/epam/hrsystem/controller/attribute/SessionAttribute.java
@@ -1,10 +1,30 @@
 package com.epam.hrsystem.controller.attribute;
 
+/**
+ * Class represents constant names of session attributes.
+ *
+ * @author Aleksey Klevitov
+ */
 public class SessionAttribute {
+    /**
+     * Represents logged user's id.
+     */
     public static final String USER_ID = "userId";
+    /**
+     * Represents current locale.
+     */
     public static final String CURRENT_LOCALE = "currentLocale";
+    /**
+     * Represents logged user's role.
+     */
     public static final String CURRENT_ROLE = "currentRole";
+    /**
+     * Represents logged user with all user's fields.
+     */
     public static final String USER = "user";
+    /**
+     * Represents all available vacancies.
+     */
     public static final String VACANCIES = "vacancies";
 
     private SessionAttribute() {
