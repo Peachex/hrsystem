e6f288926bdfae0a1c60ae813c4b64eca13eceb3
Aleksey Klevitov <klevolex@gmail.com>
2021-04-07 17:56:20 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/controller/attribute/RequestParameter.java
+++ src/main/java/com/epam/hrsystem/controller/attribute/RequestParameter.java
@@ -1,59 +1,178 @@
 package com.epam.hrsystem.controller.attribute;
 
+/**
+ * Represents constant names of request parameters.
+ *
+ * @author Aleksey Klevitov
+ */
 public class RequestParameter {
-
-    // User
+    /**
+     * The constant that represents FIRST_NAME.
+     */
     public static final String FIRST_NAME = "firstName";
+    /**
+     * The constant that represents LAST_NAME.
+     */
     public static final String LAST_NAME = "lastName";
+    /**
+     * The constant that represents DATE_OF_BIRTH.
+     */
     public static final String DATE_OF_BIRTH = "dateOfBirth";
+    /**
+     * The constant that represents PHONE_NUMBER.
+     */
     public static final String PHONE_NUMBER = "phoneNumber";
+    /**
+     * The constant that represents EMAIL.
+     */
     public static final String EMAIL = "email";
+    /**
+     * The constant that represents PASSWORD.
+     */
     public static final String PASSWORD = "password";
+    /**
+     * The constant that represents REPEATED_PASSWORD.
+     */
     public static final String REPEATED_PASSWORD = "repeatedPassword";
+    /**
+     * The constant that represents CURRENT_PASSWORD.
+     */
     public static final String CURRENT_PASSWORD = "currentPassword";
+    /**
+     * The constant that represents NEW_PASSWORD.
+     */
     public static final String NEW_PASSWORD = "newPassword";
+    /**
+     * The constant that represents REPEATED_NEW_PASSWORD.
+     */
     public static final String REPEATED_NEW_PASSWORD = "repeatedNewPassword";
+    /**
+     * The constant that represents USER.
+     */
     public static final String USER = "user";
+    /**
+     * The constant that represents USERS.
+     */
     public static final String USERS = "users";
+    /**
+     * The constant that represents USER_ID.
+     */
     public static final String USER_ID = "userId";
+    /**
+     * The constant that represents USER_ROLE.
+     */
     public static final String USER_ROLE = "userRole";
-
-    //Vacancy
+    /**
+     * The constant that represents POSITION.
+     */
     public static final String POSITION = "position";
+    /**
+     * The constant that represents DESCRIPTION.
+     */
     public static final String DESCRIPTION = "description";
+    /**
+     * The constant that represents COUNTRY.
+     */
     public static final String COUNTRY = "country";
+    /**
+     * The constant that represents CITY.
+     */
     public static final String CITY = "city";
+    /**
+     * The constant that represents VACANCIES.
+     */
     public static final String VACANCIES = "vacancies";
+    /**
+     * The constant that represents VACANCY_ID.
+     */
     public static final String VACANCY_ID = "vacancyId";
+    /**
+     * The constant that represents KEY_WORD.
+     */
     public static final String KEY_WORD = "keyWord";
+    /**
+     * The constant that represents SORT_SEQUENCE.
+     */
     public static final String SORT_SEQUENCE = "sortSequence";
+    /**
+     * The constant that represents VACANCY.
+     */
     public static final String VACANCY = "vacancy";
+    /**
+     * The constant that represents EMPLOYEE_VACANCIES.
+     */
     public static final String EMPLOYEE_VACANCIES = "employeeVacancies";
-
-    //Vacancy request
+    /**
+     * The constant that represents SUMMARY.
+     */
     public static final String SUMMARY = "summary";
-
-    //Applicant requests
+    /**
+     * The constant that represents APPLICANT_REQUESTS.
+     */
     public static final String APPLICANT_REQUESTS = "applicantRequests";
+    /**
+     * The constant that represents APPLICANT_REQUEST.
+     */
     public static final String APPLICANT_REQUEST = "applicantRequest";
+    /**
+     * The constant that represents APPLICANT_ID.
+     */
     public static final String APPLICANT_ID = "applicantId";
+    /**
+     * The constant that represents INTERVIEW_RESULT_RATING.
+     */
     public static final String INTERVIEW_RESULT_RATING = "rating";
+    /**
+     * The constant that represents INTERVIEW_RESULT_COMMENT.
+     */
     public static final String INTERVIEW_RESULT_COMMENT = "comment";
+    /**
+     * The constant that represents APPLICANT_STATE.
+     */
     public static final String APPLICANT_STATE = "applicantState";
+    /**
+     * The constant that represents TECHNICAL_INTERVIEW_DATE.
+     */
     public static final String TECHNICAL_INTERVIEW_DATE = "technicalInterviewDate";
-
-    //User report
+    /**
+     * The constant that represents USER_REPORT_SUBJECT.
+     */
     public static final String USER_REPORT_SUBJECT = "subject";
+    /**
+     * The constant that represents USER_REPORT_COMMENT.
+     */
     public static final String USER_REPORT_COMMENT = "comment";
+    /**
+     * The constant that represents REPORT.
+     */
     public static final String REPORT = "report";
+    /**
+     * The constant that represents REPORTS.
+     */
     public static final String REPORTS = "reports";
+    /**
+     * The constant that represents REPORT_ID.
+     */
     public static final String REPORT_ID = "reportId";
+    /**
+     * The constant that represents USER_REPORT_RESPONSE.
+     */
     public static final String USER_REPORT_RESPONSE = "response";
-
-    //Different
+    /**
+     * The constant that represents NEW_LOCALE.
+     */
     public static final String NEW_LOCALE = "newLocale";
+    /**
+     * The constant that represents HEADER_REFERER.
+     */
     public static final String HEADER_REFERER = "REFERER";
+    /**
+     * The constant that represents FILE_UPLOADING.
+     */
     public static final String FILE_UPLOADING = "file";
+    /**
+     * The constant that represents FILE_NAME.
+     */
     public static final String FILE_NAME = "fileName";
 
     private RequestParameter() {
