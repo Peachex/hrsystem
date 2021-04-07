76ed7c1fbb7a4474d0191b0e4749bc423513bbc0
Aleksey Klevitov <klevolex@gmail.com>
2021-04-07 17:38:29 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/controller/attribute/PagePath.java
+++ src/main/java/com/epam/hrsystem/controller/attribute/PagePath.java
@@ -1,18 +1,62 @@
 package com.epam.hrsystem.controller.attribute;
 
+/**
+ * Class represents constant paths of JSPs.
+ *
+ * @author Aleksey Klevitov
+ */
 public class PagePath {
+    /**
+     * Represents the path of login.jsp.
+     */
     public static final String LOGIN = "/WEB-INF/jsp/authorization/login.jsp";
+    /**
+     * Represents the path of vacancy_list.jsp.
+     */
     public static final String VACANCY_LIST = "/WEB-INF/jsp/vacancy/vacancy_list.jsp";
+    /**
+     * Represents the path of register.jsp.
+     */
     public static final String REGISTER = "/WEB-INF/jsp/authorization/register.jsp";
+    /**
+     * Represents the path of vacancy_info.jsp.
+     */
     public static final String CURRENT_VACANCY_INFO = "/WEB-INF/jsp/vacancy/vacancy_info.jsp";
+    /**
+     * Represents the path of employee_vacancy_info.jsp.
+     */
     public static final String EMPLOYEE_CURRENT_VACANCY_INFO = "/WEB-INF/jsp/employee/employee_vacancy_info.jsp";
+    /**
+     * Represents the path of employee_vacancy_list.jsp.
+     */
     public static final String EMPLOYEE_VACANCY_LIST = "/WEB-INF/jsp/employee/employee_vacancy_list.jsp";
+    /**
+     * Represents the path of profile.jsp.
+     */
     public static final String USER_PROFILE = "/WEB-INF/jsp/user/profile.jsp";
+    /**
+     * Represents the path of applicant_requests.jsp.
+     */
     public static final String APPLICANT_REQUESTS = "/WEB-INF/jsp/applicant/applicant_requests.jsp";
+    /**
+     * Represents the path of employee_applicant_request.jsp.
+     */
     public static final String EMPLOYEE_APPLICANT_REQUEST = "/WEB-INF/jsp/employee/employee_applicant_request.jsp";
+    /**
+     * Represents the path of user_list.jsp.
+     */
     public static final String ADMIN_USER_LIST = "/WEB-INF/jsp/admin/user_list.jsp";
+    /**
+     * The constant Represents the path of user_info.jsp.
+     */
     public static final String ADMIN_USER_INFO = "/WEB-INF/jsp/admin/user_info.jsp";
+    /**
+     * The constant Represents the path of user_report_list.jsp.
+     */
     public static final String ADMIN_USER_REPORT_LIST = "/WEB-INF/jsp/admin/user_report_list.jsp";
+    /**
+     * The constant Represents the path of user_report_info.jsp.
+     */
     public static final String ADMIN_USER_REPORT_INFO = "/WEB-INF/jsp/admin/user_report_info.jsp";
 
     private PagePath() {
