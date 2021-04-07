408d334e7e7f68c360d42daa23593a0f3e8be662
Aleksey Klevitov <klevolex@gmail.com>
2021-04-07 18:03:34 +0300

    add documentation

--- src/main/java/com/epam/hrsystem/controller/command/ActionCommand.java
+++ src/main/java/com/epam/hrsystem/controller/command/ActionCommand.java
@@ -5,6 +5,19 @@ import com.epam.hrsystem.exception.CommandException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Interface that represents "Command" pattern. Used by a controller.
+ *
+ * @author Aleksey Klevitov
+ */
 public interface ActionCommand {
+    /**
+     * Processes a request from controller and returns the page to be redirected.
+     *
+     * @param request  Request object.
+     * @param response Response object.
+     * @return CommandResult object.
+     * @throws CommandException if an exception has occurred while executing.
+     */
     CommandResult execute(HttpServletRequest request, HttpServletResponse response) throws CommandException;
 }
